077a38e36d8d2d11929f02e735e4c0076b5d31e2
David Barnett <cdavidb82@gmail.com>
2024-08-29 22:51:03 +0000

    Completed Ch1

--- Start/Ch1/basicdata.py
+++ Start/Ch1/basicdata.py
@@ -9,10 +9,20 @@ with open("../../sample-weather-history.json", "r") as weatherfile:
     weatherdata = json.load(weatherfile)
 
 # TODO: What was the warmest day in the data set?
-
+# print(len(weatherdata))
 
 # TODO: What was the coldest day in the data set?
-
+# pprint.pp(weatherdata[0])
 
 # TODO: How many days had snowfall?
 
+years = {}
+
+for d in weatherdata:
+    key = d['date'][0:4]
+    if key in years:
+        years[key] += 1
+    else:
+        years[key] = 1
+
+pprint.pp(years, width=4)    
